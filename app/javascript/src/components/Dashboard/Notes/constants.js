b867ba4dd7146c784f1a06800497dc811565afa6
Chirag bagde <chiragsbagde@gmail.com>
2022-09-21 19:00:04 +0530

    added new note page UI

--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -3,12 +3,9 @@ import * as yup from "yup";
 export const NOTES_FORM_INITIAL_FORM_VALUES = {
   title: "",
   description: "",
+  tags: [],
+  role: null,
 };
-
-export const NOTES_FORM_VALIDATION_SCHEMA = yup.object().shape({
-  title: yup.string().required("Title is required"),
-  description: yup.string().required("Description is required"),
-});
 
 export const NOTES_TABLE_COLUMN_DATA = [
   {
@@ -55,3 +52,62 @@ export const NOTES_DATA = [
     id: 4,
   },
 ];
+
+export const TAGS = [
+  {
+    label: "Getting Started",
+    value: 1,
+  },
+  {
+    label: "Onboarding",
+    value: 2,
+  },
+  {
+    label: "UX",
+    value: 3,
+  },
+  {
+    label: "bugs",
+    value: 4,
+  },
+];
+
+export const ROLES = [
+  {
+    label: "User",
+    value: "user",
+  },
+  {
+    label: "Admin",
+    value: "admin",
+  },
+  {
+    label: "Guest",
+    value: "guest",
+  },
+];
+
+export const NOTES_FORM_VALIDATION_SCHEMA = yup.object().shape({
+  title: yup.string().required("Title is required"),
+  description: yup.string().required("Description is required"),
+  role: yup
+    .object()
+    .nullable()
+    .shape({
+      label: yup.string().oneOf(ROLES.map(tag => tag.label)),
+      value: yup.string().oneOf(ROLES.map(tag => tag.value)),
+    })
+    .required("Role is required"),
+  tags: yup
+    .array(
+      yup
+        .object()
+        .nullable()
+        .shape({
+          label: yup.string().oneOf(TAGS.map(tag => tag.label)),
+          value: yup.number().oneOf(TAGS.map(tag => tag.value)),
+        })
+    )
+    .min(1, "Tag is required")
+    .required("Tag is required"),
+});
