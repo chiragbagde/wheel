4e43f7604cc22a3f5defb27dff2ec8b572c0c1e8
Chirag bagde <chiragsbagde@gmail.com>
2022-09-28 14:58:08 +0530

    updated message from delete alert

--- app/javascript/src/components/Dashboard/Contacts/DeleteAlert.jsx
+++ app/javascript/src/components/Dashboard/Contacts/DeleteAlert.jsx
@@ -5,7 +5,7 @@ import { Alert, Toastr } from "neetoui";
 const DeleteAlert = ({ onClose }) => (
   <Alert
     isOpen
-    message="Are you sure you want to continue? This cannot be undone."
+    message="Are you sure you want to continue deleting this contact? This cannot be undone."
     title="Delete Contact"
     onClose={onClose}
     onSubmit={() => {
